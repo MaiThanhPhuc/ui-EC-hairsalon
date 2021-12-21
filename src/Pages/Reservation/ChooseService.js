7f358fc8aa260d725cfd51acf767f8f9457ca784
kizu <yzukihatama@gmail.com>
2021-12-21 15:13:37 +0700

    Fix

--- src/Pages/Reservation/ChooseService.js
+++ src/Pages/Reservation/ChooseService.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react'
-
-
-import AuthService from '../../Services/auth.service'
 import API from '../../Services/api'
 import Storage from '../../Services/storage';
 import Navbar from '../../Components/Navbar/Navbar'
 import Footer from '../../Components/Footer/Footer'
 
-
-
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-
-import {
-    Container,
-    Paper,
-    Grid,
-    Typography,
-    ButtonBase,
-    IconButton,
-    Button,
-    Card,
-    Avatar,
-    Box
-} from '@mui/material';
-
+import {  Container, Paper, Grid, Typography, ButtonBase, IconButton, Button, Card,Box } from '@mui/material';
+import { BookOnline } from '@mui/icons-material/BookOnline'
 
 export class ChooseService extends Component {
     constructor(props) {
@@ -77,30 +59,87 @@ export class ChooseService extends Component {
         return (
             <>
                 <Navbar />
-                <Container maxWidth="sm" sx={{ mb: 4 }} >
-                    <Paper sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                <Container className='booking-service' maxWidth="sm" sx={{ mb: 4 }} >
+                    <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                         <Button fullWidth color='primary' variant="outlined" sx={{ mt: 2 }} onClick={(e) => this.onClickForwardToSlot(choosenService)}>
                             Chọn ({choosenService.length}) dịch vụ
                         </Button>
-                        <div style={{color:"violet"}}>
-                            {
-                                services.map((service, index) => {
-                                    if (service.category.id == 5)
-                                        return (
-                                            <Paper variant="outlined" elevation="16" key={index} sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 }}}>
-                                                <Grid container>
-                                                    <Grid item>
-                                                        <Avatar src={service.image} sx={{height:"56px", width:"56px"}}/>
-                                                    </Grid>
-                                                    <Grid item>
-
-                                                    </Grid>
+                        {
+                            services.map((service) => {
+                                return (
+                                    <Container maxWidth="sm" sx={{ mb: 4 }} >
+                                        <Box  sx={ {  boxShadow: "1px 3px 1px #cccccc", border: '1px solid #cccccc', my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 },   display: "flex",flexDirection:"column", alignItems:"center",justifyContent:"center" }}>
+                                            <div>
+                                                <ButtonBase onClick={() => { }} sx={{
+                                                    width: 180,
+                                                    height: 180,
+                                                    marginRight: "1.5em",
+                                                }}>
+                                                    <img alt="complex" src={service.image} style={{
+                                                        margin: "auto",
+                                                        display: "block",
+                                                        width: 250,
+                                                        height: 180,
+                                                        maxWidth: "100%",
+                                                        maxHeight: "100%",
+                                                        objectFit:"contain"
+                                                    }} />
+                                                </ButtonBase>
+                                            </div>
+                                            <div>
+                                                <Grid item xs container direction="column" spacing={2}>
+                                                    <div style={{
+                                                            width: "180px"
+                                                    }}>
+                                                        <Typography
+                                                            variant="h6"
+                                                            gutterBottom
+                                                            style={{
+                                                                fontWeight:"300",
+                                                                fontSize:"16px"
+                                                            }}
+                                                        >
+                                                            {service.name}
+                                                        </Typography>
+                                                        <Typography
+                                                            variant="body2"
+                                                            color="textSecondary"
+                                                            style={{
+                                                                display: "flex",
+                                                                flexDirection: "row",
+                                                                alignItems: "center",
+                                                                justifyContent: "flex-start",
+                                                            }}
+                                                        >
+                                                            {service.address}
+                                                        </Typography>
+                                                    </div>
                                                 </Grid>
-                                            </Paper>
-                                        )
-                                })
-                            }
-                        </div>
+                                            </div>
+                                            <div>
+                                                <Button
+                                                    //key={index}
+                                                    size="small"
+                                                    color="primary"
+                                                    variant="contained"
+                                                    style={{ marginRight: ".5em" }}
+                                                    //disabled={isDisabled}
+                                                    onClick={(e) => this.chooseService(service)}
+                                                >
+                                                    Chọn dịch vụ
+                                                </Button>
+                                                <IconButton
+                                                    aria-label="delete"
+                                                    size="small"
+                                                >
+                                                </IconButton>
+                                            </div>
+                                        </Box >
+                                    </Container>
+                                )
+                            })
+                        }
+
                     </Paper>
                 </Container>
                 <Footer />
