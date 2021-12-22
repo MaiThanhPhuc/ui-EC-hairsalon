768130e0b061f5188a2c5463e13d0041ffadae5e
kizu08 <yzukihatama@gmail.com>
2021-12-22 14:56:55 +0700

    ADMIN

--- src/Components/Admin/topbar/Topbar.js
+++ src/Components/Admin/topbar/Topbar.js
@@ -1,13 +1,18 @@
 import React from "react";
-import "./topbar.css";
-import { Settings } from "@mui/icons-material";
+
+import { Link } from "react-router-dom";
+
+import "./Topbar.css"
+
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">Admin Page</span>
+          <Link to="/admin">
+            <span className="logo">Admin Page</span>
+          </Link>
         </div>
       </div>
     </div>
